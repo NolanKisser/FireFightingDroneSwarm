63a82632806f49748c3670c049617d8d863e7711
Celina Yang <celinayang18@hotmail.com>
2026-01-26 13:53:20 -0500

    updated run() in DroneSubsystem to receive and complete fire events

--- src/DroneSubsystem.java
+++ src/DroneSubsystem.java
@@ -1,7 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class DroneSubsystem implements Runnable {
+
+    private final Scheduler scheduler;
+    private final int droneID;
+
+    /**
+     * Constructor
+     * @param scheduler
+     * @param droneID
+     */
+    public DroneSubsystem(Scheduler scheduler, int droneID) {
+        this.scheduler = scheduler;
+        this.droneID = droneID;
+    }
 
     @Override
     public void run() {
+        while(true) {
+            FireEvent event = scheduler.getNextFireEvent();
+            if (event == null) {
+                break;
+            }
+            System.out.println("DroneID: " + droneID + " extinguishing event: " + event);
 
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            scheduler.completeFireEvent(event);
+            System.out.println("DroneID: " + droneID + " extinguished event: " + event);
+        }
     }
 }
