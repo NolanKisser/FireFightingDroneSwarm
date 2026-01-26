0bdc485e8e92e886d122973a962ea5e49c4401c6
Celina Yang <celinayang18@hotmail.com>
2026-01-26 13:22:35 -0500

    (incomplete) implemented newFireEvent, getNextFireEvent, and completeFireEvent

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,4 +1,42 @@
+import java.util.*;
+
 public class Scheduler {
+
+    private final Queue<FireEvent> incompleteEvents = new LinkedList<>();
+    private final Queue<FireEvent> completeEvents = new LinkedList<>();
+
+
+    /**
+     * Fire event from the csv file gets added to the incompleteEvents list
+     * @param fireEvent
+     */
+    public synchronized void newFireEvent(FireEvent fireEvent) {
+        incompleteEvents.add(fireEvent);
+        notifyAll();
+    }
+
+    /**
+     * Retrieve the next fire event for a firefighter drone to extinguish
+     */
+    public synchronized void getNextFireEvent() {
+        while(incompleteEvents.isEmpty()){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Completed fire event gets added to the completeEvents list
+     * @param fireEvent
+     */
+    public synchronized void completeFireEvent(FireEvent fireEvent) {
+        completeEvents.add(fireEvent);
+        notifyAll();
+    }
+
 
 
 }
