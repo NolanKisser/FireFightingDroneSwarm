625b4621831254309b3b40b78c71f526f9b6d333
Celina Yang <celinayang18@hotmail.com>
2026-01-27 22:20:16 -0500

    update Scheduler and add javadocs

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,14 +1,22 @@
 import java.util.*;
 
+/**
+ * Scheduler class communicates and synchronizes the FireIncidentSubsystem and the DroneSubsystem.
+ * @author Jordan Grewal, Ozan Kaya, Nolan Kisser, Celina Yang
+ * @version January 31, 2026
+ */
 public class Scheduler {
 
+    // fire events to be completed
     private final Queue<FireEvent> incompleteEvents = new LinkedList<>();
+    // completed fire events
     private final Queue<FireEvent> completeEvents = new LinkedList<>();
 
+    private boolean allEventsDone = false;
 
     /**
-     * Fire event from the csv file gets added to the incompleteEvents list
-     * @param fireEvent
+     * Adds a new fire event from the CSV file to the incomplete events list
+     * @param fireEvent event to add
      */
     public synchronized void newFireEvent(FireEvent fireEvent) {
         incompleteEvents.add(fireEvent);
@@ -17,15 +25,23 @@ public class Scheduler {
 
     /**
      * Retrieve the next fire event for a firefighter drone to extinguish
+     * If there are no fire events available, drone thread is blocked until an event is submitted
+     * or until all events are complete
      */
     public synchronized FireEvent getNextFireEvent() {
-        while(incompleteEvents.isEmpty()){
+
+        while(incompleteEvents.isEmpty() && !allEventsDone) {
             try {
                 wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+
+        if(incompleteEvents.isEmpty() && allEventsDone) {
+            return null;
+        }
+
         return incompleteEvents.remove();
     }
 
